540161854bf0a07c67bf3952ad3361169bab414b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-08-15 20:10:05 +0530

    add content.

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,3 +1,38 @@
 <template>
-    <div></div>
+    <UDivider icon="i-simple-icons-github" />
+    <UCard>
+        <template #footer>
+            <div class="content">
+                <UCard>
+                    About
+
+                </UCard>
+                <div class="socials">
+                    <a href="https://github.com/Nayantha"><UIcon class="w-5 h-5 text-primary-500" name="i-mdi-linkedin"/></a>
+                    <a href="https://www.linkedin.com/in/nayantha-yasiru"><UIcon class="w-5 h-5 text-primary-500" name="i-mdi-github"/></a>
+                </div>
+            </div>
+            <div class="copyright-text">
+                &copy; 2024 Nayantha Koralegama
+            </div>
+        </template>
+    </UCard>
 </template>
+<style scoped>
+    .content {
+        display: flex;
+        justify-content: space-around;
+    }
+    .socials {
+        width: 150px;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+    .copyright-text {
+        display: flex;
+        justify-content: center;
+    }
+</style>
+<script lang="ts" setup>
+</script>
