905bdfb639a9ce69071b1a164424b17c97ed5a1d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-06 21:06:10 +0530

    add page to view transactions.

---
+++ pages/transactions/view.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Transactions for Account ID: {{ accountId }}</h1>
+        <ul>
+            <li v-for="transaction in transactions" :key="transaction.id">
+                {{ transaction.type }} - {{ transaction.amount }} - {{ transaction.note }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import {onMounted, ref} from 'vue';
+import {useRoute} from 'vue-router';
+
+const route = useRoute();
+const accountId = ref(route.params.accountId);
+const transactions = ref([]);
+
+onMounted(async () => {
+    transactions.value = await $fetch(`/api/transactions?accountId=${accountId.value}`);
+});
+</script>
+
+
+<style scoped>
+
+</style>
