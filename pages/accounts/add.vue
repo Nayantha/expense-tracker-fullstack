1223885ea2d9a717918e79ea45c54c51135b318f
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-06 20:37:13 +0530

    add component to add account.

---
+++ pages/accounts/add.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <form @submit.prevent="createAccount">
+            <input v-model="no" placeholder="Account Number" />
+            <input v-model="bankName" placeholder="Bank Name" />
+            <input v-model="accountType" placeholder="Account Type" />
+            <input v-model="interrestRate" placeholder="Interest Rate" type="number" />
+            <button type="submit">Create Account</button>
+        </form>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+const no = ref('');
+const bankName = ref('');
+const accountType = ref('');
+const interrestRate = ref(0);
+
+async function createAccount() {
+    await $fetch('/api/accounts', {
+        method: 'POST',
+        body: {
+            no: no.value,
+            bankName: bankName.value,
+            accountType: accountType.value,
+            interrestRate: interrestRate.value,
+        },
+    });
+}
+</script>
