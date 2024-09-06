a9af8d10701b1fc7a1b280371663241918a6ffd6
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-06 21:05:44 +0530

    add page to view accounts.

---
+++ pages/accounts/view.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Accounts</h1>
+        <ul>
+            <li v-for="account in accounts" :key="account.id">
+                {{ account.bankName }} - {{ account.no }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import {onMounted, ref} from 'vue';
+
+const accounts = ref([]);
+
+onMounted(async () => {
+    accounts.value = await $fetch('/api/accounts');
+});
+</script>
+
+<style scoped>
+
+</style>
