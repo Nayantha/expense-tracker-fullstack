a3b5c59df394c4ce806e393f44d0109dfcb3e15f
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-10 22:28:28 +0530

    change the script language to typescript and add types.

--- pages/transactions/[index].vue
+++ pages/transactions/[index].vue
@@ -9,13 +9,14 @@
     </div>
 </template>
 
-<script setup>
-import {onMounted, ref} from 'vue';
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+import { Transaction } from "~/models/Transaction";
 
-const transactions = ref([]);
+const transactions = ref<Transaction[]>([]);
 
 onMounted(async () => {
-    transactions.value = await $fetch(`/api/transactions`, { method: "GET"});
+    transactions.value = await $fetch(`/api/transactions`, { method: "GET" });
 });
 </script>
 
