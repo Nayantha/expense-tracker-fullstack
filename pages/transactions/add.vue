c38bee4ac032848e699962b7f17d77e78fd6738d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-10 22:15:24 +0530

    change the script language to typescript and add types.

--- pages/transactions/add.vue
+++ pages/transactions/add.vue
@@ -3,20 +3,22 @@
         <form @submit.prevent="createTransaction">
             <input v-model="accountId" placeholder="Account ID" />
             <input v-model="amount" placeholder="Amount" type="number" />
-            <input v-model="type" placeholder="Type (deposit/withdrawal)" />
+            <input v-model="type" placeholder="Type" />
             <input v-model="note" placeholder="Note" />
             <button type="submit">Create Transaction</button>
         </form>
     </div>
 </template>
 
-<script setup>
-import {ref} from 'vue';
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { AccountType } from "~/models/AccountType";
 
-const accountId = ref('');
-const amount = ref(0);
-const type = ref('');
-const note = ref('');
+
+const accountId = ref<string>("");
+const amount = ref<Number>(0.0);
+const type = ref<AccountType>(AccountType.Savings);
+const note = ref<string>('');
 
 async function createTransaction() {
     await $fetch('/api/transactions', {
