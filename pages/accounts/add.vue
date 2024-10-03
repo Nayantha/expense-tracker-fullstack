ad5e9cec1e2703c23c6290c17e054b1d83d17e1a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-10-03 21:23:16 +0530

    use nuxt ui component.

--- pages/accounts/add.vue
+++ pages/accounts/add.vue
@@ -1,22 +1,37 @@
 <template>
     <div>
-        <form @submit.prevent="createAccount">
-            <input v-model="accountNumber" placeholder="Account Number" />
-            <input v-model="bankName" placeholder="Bank Name" />
-            <select v-model="accountType">
-                <option v-for="(accountType, key) in accountTypeOptions" :key="key" :value="accountType">
-                    {{ accountType }}
-                </option>
-            </select>
+        <UForm :schema="schema" :state="state" class="space-y-4" @submit.prevent="createAccount">
+            <UFormGroup label="Account Number" required>
+                <UInput v-model="accountNumber" icon="i-heroicons-envelope" placeholder="Account Number"/>
+            </UFormGroup>
+<!--            :error="!bankName && 'You must enter an bank name'"-->
+            <UFormGroup v-slot="{ error }"
+                        description="We'll only use this for spam."
+                        help="We will never share your email with anyone else."
+                        hint="Optional"
+                        label="Bank Name" required size="xl">
+                <UInput v-model="state.bankName" :trailing-icon="error ? 'i-heroicons-exclamation-triangle-20-solid' : ''" icon="i-heroicons-envelope" placeholder="Bank Name"/>
+            </UFormGroup>
+            <USelect v-model="accountType" :options="accountTypeOptions" placeholder="Select account type"/>
             <input v-model="interestRate" placeholder="Interest Rate" type="number" />
             <button type="submit">Create Account</button>
-        </form>
+        </UForm>
     </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
 import { AccountType } from "~/models/AccountType";
+
+import { z } from 'zod';
+
+const schema = z.object({
+    backName: z.string().min(10, 'Must be at least 10 characters')
+})
+
+const state = reactive({
+    backName: undefined
+})
 
 const accountTypeOptions = Object.values(AccountType);
 const accountNumber = ref<string>('');
